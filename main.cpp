57eb3f46b2aaeace2ff71af91ed49e2bcbf8d8d6
Jinzhe Zhang <sadvent@MacBook-Air.local>
2016-03-13 16:43:46 +0100

    Add to main all constructor's test code

--- main.cpp
+++ main.cpp
@@ -4,33 +4,60 @@
 #include "String.h"
 
 int main(int argc, char* argv[]){
-    //printf("Hello\n");
-    //printf("Hello\n"); //1 alloc not freed
     //Test Constructors:
-    printf("==Test defaut constructor String()==\n");
+    printf("==Test of defaut constructor String()==\n");
     String h1=String();
     printf("String h1 delarated with String h=String()\n");
     printf("c_str: %s \n",h1.c_str());
     printf("Length: %zu \n", h1.length());
-    printf("Capacity: %zu \n",h1.capacity());
+    printf("Capacity: %zu \n\n",h1.capacity());
     
+	printf("==Test of fill constructor String(size_t n, char c)==\n");
     String h2=String(5,'x');
     printf("String h2 delarated with String h2=String(5,'x')\n");
     printf("c_str: %s \n",h2.c_str());
     printf("Length: %zu \n", h2.length());
-    printf("Capacity: %zu \n",h2.capacity());
+    printf("Capacity: %zu \n\n",h2.capacity());
     
     String h3=String(55,'x');
 	printf("String h3 delarated with String h3=String(55,'x')\n");
 	printf("c_str: %s \n",h3.c_str());
     printf("Length: %zu \n", h3.length());
-    printf("Capacity: %zu \n",h3.capacity());
+    printf("Capacity: %zu \n\n",h3.capacity());
     
-    String h4=String(155,'x');
 	printf("String h4 delarated with String h4=String(155,'x')\n");
+    String h4=String(155,'x');
     printf("c_str: %s \n",h4.c_str());    
     printf("Length: %zu \n", h4.length());
-    printf("Capacity: %zu \n",h4.capacity());
+    printf("Capacity: %zu \n\n",h4.capacity());
+    
+    printf("==Test of Copy Constructor String(const String& str)==\n");
+    String h5=String(h1);
+    printf("String h5 delarated with String h5=String(h1)\n");
+    printf("c_str: %s \n",h5.c_str());
+    printf("Length: %zu \n", h5.length());
+    printf("Capacity: %zu \n\n",h5.capacity());
+    //printf("sizeof %lu\n",sizeof(h4.c_str()));
+    String h6=String(h4);
+    printf("String h6 delarated with String h6=String(h4)\n");
+    printf("c_str: %s \n",h6.c_str());
+    printf("Length: %zu \n", h6.length());
+    printf("Capacity: %zu \n\n",h6.capacity());
+	
+	char c1[]= "abcdefg";
+	String h7=String(c1);
+    printf("String h7 delarated with String h7=String(c1)\n");
+    printf("c_str: %s \n",h7.c_str());
+    printf("Length: %zu \n", h7.length());
+    printf("Capacity: %zu \n\n",h7.capacity());
+    
+    char c2[]= "12345678901234567890123456789012345678901234567890"
+    "123456789012345678901234567890123456789012345678901234567890";
+	String h8=String(c2);
+    printf("String h8 delarated with String h8=String(c2)\n");
+    printf("c_str: %s \n",h8.c_str());
+    printf("Length: %zu \n", h8.length());
+    printf("Capacity: %zu \n\n",h8.capacity());
     //String h2(5,'x');
     //String h3=h2;
     ////printf ("%s \n",h3.c_str());
